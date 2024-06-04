c8812d225bc514c0aa8eba87fa0439b6dc7614a5
unknown <zahidan23@gmail.com>
2024-06-04 16:53:52 +0700

    first push

---
+++ app/Models/Post.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Post extends Model
+{
+    use HasFactory;
+    protected $fillable = ['title', 'author_id', 'slug', 'body'];
+
+    //untuk menghubungkan table post dengan table user (Defining the Inverse of the Relationship)
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
